242545c598b570a6f90d0d6f00fc0b9079640eff
MatiasBahamonde <matiasalejandro.bahamonde@alumnos.ulagos.cl>
2024-11-05 11:25:36 -0300

    Update asyncMock.jsx

--- src/Data/asyncMock.jsx
+++ src/Data/asyncMock.jsx
@@ -4,14 +4,16 @@ const products = [
     name: 'Camiseta Visitante Chile 2024',
     description: 'La camiseta visitante de la selección chilena 2024 presenta un elegante diseño en azul oscuro, complementado con detalles en rojo y blanco que reflejan la identidad nacional.',
     price: 30000,
-    image: 'public/ChileVisitante',
+    image: 'public/ChileVisitante.jpg',
+    category: 'Chile'
   },
   {
     id: 2,
     name: 'Camiseta Local Chile 2024',
     description: 'La camiseta de la selección chilena 2024 resalta en rojo vibrante, con detalles en azul y blanco que representan la bandera nacional.El escudo de la Asociación Nacional de Fútbol Profesional (ANFP).',
     price: 32000,
-    image: 'public/Chilelocal',
+    image: 'public/Chilelocal.jpg',
+    category: 'Chile'
   },
   {
     id: 3,
@@ -19,20 +21,23 @@ const products = [
     description: 'La camiseta de la selección argentina 2024 destaca por sus colores celeste y blanco, representando la bandera nacional. ',
     price: 36000, 
     image: 'public/Argentina.jpg',
+    category: 'Argentina'
   },
   {
     id: 4,
     name: 'Camiseta Local Colo Colo 2024',
     description: 'La camiseta local de Colo Colo 2024 destaca por su clásico diseño blanco, que simboliza la tradición y la historia del club. Con un corte moderno y detalles en negro y rojo.',
     price: 30000,
-    image: 'public/colovisitante',
+    image: 'public/colovisitante.jpg',
+    category: 'Colo Colo'
   },
   {
     id: 5,
     name: 'Camiseta Visita Colo Colo 2024',
     description: 'La camiseta visitante de Colo Colo para la temporada 2024 presenta un diseño fresco y elegante, con un elegante color negro que resalta la identidad del club.',
     price: 29000,
-    image: 'public/cololocal',
+    image: 'public/cololocal.jpg',
+    category: 'Colo Colo'
   },
   {
     id: 6,
@@ -40,6 +45,8 @@ const products = [
     description: 'La camiseta local de Flamengo 2024 se caracteriza por su icónico diseño en franjas horizontales rojas y negras, representando los colores tradicionales del club.',
     price: 30000,
     image: 'public/Flamengo.jpg',
+    category: 'Flamengo'
+    
   },
 ];
 
